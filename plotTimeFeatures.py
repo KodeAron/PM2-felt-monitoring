4675f2af4e6eb43183978917dcd0748c644223fd
Aron <67368318+CodeAron@users.noreply.github.com>
2021-02-22 16:19:03 +0100

    init

---
+++ plotTimeFeatures.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pyuff
+uff_file = pyuff.UFF('data/beam.uff')
+# uff_file = pyuff.UFF('../Tidssignaler_t.o.m._210221/P001F_A_201027-210221.uff')
