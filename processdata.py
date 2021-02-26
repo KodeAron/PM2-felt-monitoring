6df094ba758567d792fdfb2f492c11611c4fb0a5
Aron <67368318+CodeAron@users.noreply.github.com>
2021-02-26 20:49:31 +0100

    if main etc.

--- processdata.py
+++ processdata.py
@@ -4,3 +4,18 @@
 Created on Feb 25 2021 11:30
 @author: Aron, Luleå University of Technology
 """
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+
+def main():
+    filename = '../data_felt/' + 'Filtbyten.xlsx'
+
+
+
+def readProcessData(filename)
+    # extract
+    return # dataframe
+
+if __name__ == '__main__':
+    main()
