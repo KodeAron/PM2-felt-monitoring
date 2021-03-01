c95550c4961478e0258e08d64fed906fca980b0c
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-01 10:05:50 +0100

    Script for plotting and merging the different dataframe datasets.

---
+++ datamerger.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- python3
+""" Merge and plot dataframes of accelerometer, felt, process, operation data
+
+Read acceleration data in UFF format. Return dataframe or matrix. Save to csv.
+
+Created on March 1 2021 10:03
+@author: Aron, Luleå University of Technology
+"""
