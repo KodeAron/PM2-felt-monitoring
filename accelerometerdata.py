08172e55883032d0d3c5e1efd6c5d6dfacb25418
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-01 09:44:53 +0100

    Changed file location. Fixed bug of hard coding filename.

--- accelerometerdata.py
+++ accelerometerdata.py
@@ -41,12 +41,12 @@ def dataUFF_to_featuresCSV(sensorPosition, timePeriod):
     featuresDF.to_csv(csvfilename)
 
 def dataUFF_to_featuresDF(sensorPosition, timePeriod):
-    data = readUFF('../' + sensorPosition + '_A_' + timePeriod + '.uff')
+    data = readUFF('../data_observer/' + sensorPosition + '_A_' + timePeriod + '.uff')
     featuresDF = featuresAsDataframe(data)
     return featuresDF
 
 def readUFF(filename):
-    uff_file = pyuff.UFF('../P001F_A_201027-210221.uff') # Tidssignaler_t.o.m._210221/
+    uff_file = pyuff.UFF(filename) # Tidssignaler_t.o.m._210221/
     # uff_types = uff_file.get_set_types()
     # print(uff_types)
     # uff_set1 = uff_file.read_sets(0)
