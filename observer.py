e0c8c8b90e820b059fa51baa7a5e23a629ba66bb
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-02 11:30:02 +0100

    dataframe with raw data. Frame for load and convert functions.
    The features dataframe now contains also 'x' and 'data' from the UFF.
    Started construting functions for convert several data files at once and load several stored CSV files.
    The load function should check for existing CSV files otherwise run convert function.
    Changed method names to snake_case according to convention.

--- observer.py
+++ observer.py
@@ -21,8 +21,17 @@ def main():
     featuresDF = dataUFF_to_featuresDF(sensorPosition, timePeriod)
     # print(data[0])
     # # plotSignal(data, 4)
-    # print(featuresDF.loc[4])
+    print(featuresDF.loc[4])
     plotFeatures(featuresDF)
+    
+def load_data(sensorPositions=[]):
+# Load data from CSVs if available. Return list of dataframes, one for each position.
+    return listOfDataframes
+
+def UFF_to_CSV(sensorPositions=[],timePeriod=''):
+# load UFF, convert to dataframe (with only interesting fields) and save to CSV
+# sensorPositions: list of sensor positions that should be retrieved
+    pass
 
 def dataUFF_to_featuresCSV(sensorPosition, timePeriod):
     featuresDF = dataUFF_to_featuresDF(sensorPosition, timePeriod)
@@ -34,21 +43,21 @@ def dataUFF_to_featuresDF(sensorPosition, timePeriod):
     featuresDF = featuresAsDataframe(data)
     return featuresDF
 
-def readUFF(filename):
+def read_UFF(filename):
     uff_file = pyuff.UFF(filename) # Tidssignaler_t.o.m._210221/
     # uff_set1 = uff_file.read_sets(0)
     # print(uff_set1) # print the dictionary for measurement 1
     data = uff_file.read_sets()
     return data
 
-def plotSignal(data, index):
+def plot_signal(data, index):
     plt.semilogy(data[index]['x'], np.abs(data[index]['data']))
     plt.xlabel('Time [s]')
     plt.ylabel('Acceleration [g]')
     # plt.xlim([0,10])
     plt.show()
 
-def plotFeatures(features):
+def plot_features(features):
     if type(features) is np.array:
         plt.plot(features[:,0],'b-', label="rms")
         plt.plot(features[:,1],'r-', label="kurtosis")
@@ -63,27 +72,16 @@ def plotFeatures(features):
     else:
         print('Unknown format for features')
 
-def featuresAsMatrix(data):
+def features_dataframe(data):
     # rms, kurtosis
-    featuresMatrix = np.empty([len(data),2], dtype=float)
-
-    for i in range(len(data)):
-        elem_rms = np.sqrt(np.mean(data[i]['data']**2))
-        elem_kurtosis = spstats.kurtosis(np.abs(data[i]['data']))
-        new_row = np.array([[elem_rms, elem_kurtosis]])
-        featuresMatrix[i] = new_row
-    
-    return featuresMatrix
-
-def featuresAsDataframe(data):
-    # rms, kurtosis
-    featuresDF = pd.DataFrame(columns=['Datetime', 'RMS', 'KURT'])
+    featuresDF = pd.DataFrame(columns=['Datetime', 'RMS', 'KURT','x','data'])
 
     for i in range(len(data)):
         val_rms = np.sqrt(np.mean(data[i]['data']**2))
         val_kurtosis = spstats.kurtosis(np.abs(data[i]['data']))
         # save the calculated features in dataframe. Get datetime at id3 in data
-        featuresDF = featuresDF.append({'Datetime': data[i]['id3'], 'RMS': val_rms, 'KURT': val_kurtosis},\
+        featuresDF = featuresDF.append({'Datetime':data[i]['id3'], 
+            'RMS':val_rms, 'KURT':val_kurtosis, 'x':data[i]['x'], 'data':data[i]['data']},
             ignore_index=True)
 
     featuresDF['Datetime'] = pd.to_datetime(featuresDF['Datetime'], format='%d-%m-%Y %H:%M:%S')
