b8180b6646eedde70aca59ee89a6a905ad8267d6
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-04 16:10:12 +0100

    removed stackoverflow test function. updated some functions name according to changes in other modules

--- datamerger.py
+++ datamerger.py
@@ -17,11 +17,12 @@ import protak
 
 def main():
     # stackOverflowTest()
-    df_observer = observer.dataUFF_to_featuresDF(sensorPosition = 'P001F', timePeriod = '201027-210221')
-    df_protak = protak.readData()
-    print(df_protak.columns)
-    # plotVibNLogg(df_observer,df_protak)
-    doublePlot([df_observer,df_protak], ['RMS','KURT'],['trimproblem'])#,'massakladd'
+    df_observer = observer.load_data(positions = ['P001F'], timeperiod = '201027-210221')
+    df_protak = protak.load_data()
+    # print(df_protak.columns)
+    plotVibNLogg(df_observer,df_protak)
+    print(df_observer)
+    # doublePlot([df_observer,df_protak], ['RMS','KURT'],['trimproblem'])#,'massakladd'
 
 def plotVibNLogg(df_observer, df_protak):
 
@@ -59,47 +60,7 @@ def plotVibNLogg(df_observer, df_protak):
     plt.subplots_adjust(hspace=.0)
     plt.show()
 
-def stackOverflowTest():
-#     import matplotlib.pyplot as plt
-#     import numpy as np
-#     from matplotlib import gridspec
-
-    # Simple data to display in various forms
-    x = np.linspace(0, 2 * np.pi, 400)
-    y = np.sin(x ** 2)
-
-    fig = plt.figure()
-    # set height ratios for subplots
-    gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1]) 
-
-    # the first subplot
-    ax0 = plt.subplot(gs[0])
-    # log scale for axis Y of the first subplot
-    ax0.set_yscale("log")
-    line0, = ax0.plot(x, y, color='r')
-
-    # the second subplot
-    # shared axis X
-    ax1 = plt.subplot(gs[1], sharex = ax0)
-    line1, = ax1.plot(x, y, color='b', linestyle='--')
-    plt.setp(ax0.get_xticklabels(), visible=False)
-    # remove last tick label for the second subplot
-    yticks = ax1.yaxis.get_major_ticks()
-    yticks[-1].label1.set_visible(False)
-
-    # put legend on first subplot
-    ax0.legend((line0, line1), ('red line', 'blue line'), loc='lower left')
-
-    # remove vertical gap between subplots
-    plt.subplots_adjust(hspace=.0)
-    plt.show()
-
-def doublePlot(df_list, plt1items, plt2items):
-    # Simple data to display in various forms
-    x = np.linspace(0, 2 * np.pi, 400)
-    y = np.sin(x ** 2)
-
-    fig = plt.figure()
+def doublePlot(df_list, plt1items, plt2items):fig = plt.figure()
     # set height ratios for subplots
     gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1]) 
     # the first subplot
