306c04ed0f7b1d2336eabffcca867f0ff8bc3368
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-04 17:27:56 +0100

    convert INSTALLED and REMOVED to datetime. Recalculate RUNTIME

--- feltdata.py
+++ feltdata.py
@@ -15,11 +15,21 @@ fullpath_data = path_data + filename_data
 def main():
     df = load_data()
     print(df.columns)
+    print(df['REMOVED'])
+    print(df['INSTALLED'])
+    print(df['RUNTIME'])
+    print(type(df['REMOVED'].iloc[0]))
+    print(df['RUNTIME'].iloc[0])
 
 
-def load_data(filename=fullpath_data):
+def load_data(filename=fullpath_data,recalculate_runtime=True):
     # extract felt data from spreadsheet and return as dataframe
     df = pd.read_excel(filename)
+    # df.convert_dtypes()
+    df['INSTALLED'] = pd.to_datetime(df['INSTALLED']) #, format='%Y-%m-%d')
+    df['REMOVED'] = pd.to_datetime(df['REMOVED'])
+    if recalculate_runtime:
+        df['RUNTIME']=df['REMOVED']-df['INSTALLED']
     return df
 
 if __name__ == '__main__':
