22bc2e725974acd01077b373f72b33da1ef3593c
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-05 10:22:15 +0100

    count_breaches() fixed and default values set to -inf/inf. main() added for testing purposes

--- generaltools.py
+++ generaltools.py
@@ -1,18 +1,22 @@
 import numpy as np
+import math
 
 point_click = tuple()
+
+def main():
+    testlista = [4, 5, 6 ,1 ,-1, 8]
+    breaches = count_breaches(testlista, upper_threshold= 4)
+    print(breaches)
 
 def nearest(items, pivot):
     nearest = min(items, key=lambda x: abs(x - pivot))
     timedelta = abs(nearest - pivot)
     return nearest, timedelta
 
-def count_breaches(list_of_values, lower_threshold, upper_threshold):
-    lowerbool = list_of_values < lower_threshold
-    nBreaches_lower = sum(lowerbool)
-    upperbool = list_of_values > upper_threshold
-    nBreaches_lower = sum(upperbool)
-    return *zip(nBreaches_lower, nBreaches_upper) # vet inte riktigt vad zip gör...
+def count_breaches(list_of_values, lower_threshold=-math.inf, upper_threshold=math.inf):
+    nBreaches_lower = sum(x < lower_threshold for x in list_of_values)
+    nBreaches_upper = sum(x > upper_threshold for x in list_of_values)
+    return nBreaches_lower, nBreaches_upper
 
 def on_pick(event):
     thisline  = event.artist
@@ -48,3 +52,6 @@ def datetime64_to_datetime(np_datetime64):
 #     print('onpick points:', *zip(xdata[ind], ydata[ind]))
 #     global point_click
 #     point_click = (xdata[ind], ydata[ind])
+
+if __name__ == '__main__':
+    main()
