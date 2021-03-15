eb66d741f39869158e852c50771d973d1bc26621
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-15 13:49:46 +0100

    extract list of replacements between specified dates

--- feltdata.py
+++ feltdata.py
@@ -7,6 +7,9 @@ Created on Feb 25 2021 11:30
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
+import datetime as dt
+
+import generaltools as gtol
 
 path_data = '../data_felt/'
 filename_data = 'Press1T_felt_log.xlsx'
@@ -16,7 +19,8 @@ def main():
     df = load_data()
     print(df.columns)
     # print(df)
-    plot_feltchange(df)
+    # plot_feltchange(df)
+    print(replacement_list(df, first_date=dt.date(2020,10,20)))
 
 def load_data(filename=fullpath_data,recalculate_runtime=True):
 # extract felt data from spreadsheet and return as dataframe
@@ -32,6 +36,13 @@ def plot_feltchange(feltDF):
 # plot dates where the felt was changed
     plt.vlines(feltDF.INSTALLED, -5, 5, colors='k', linestyles='solid', label='replacements')
     plt.show()
+
+def replacement_list(feltDF,first_date=None, last_date=None):
+    if type(first_date)== None:
+        print('empty')
+    else:
+        extracted_series = gtol.extract_date_list(feltDF.INSTALLED,first_date,last_date)
+        return extracted_series
 
 if __name__ == '__main__':
     main()
