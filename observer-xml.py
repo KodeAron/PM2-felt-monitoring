d64b690c7cb138649fd08fc6835eaf8851ef8db8
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-16 13:45:03 +0100

    Fundamentals; script doc, main() etc.

---
+++ observer-xml.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*- python3
+""" Accelerometer data reader
+
+Read acceleration data in xml (xmd/xme) format. Return dataframe. Save to file.
+
+Created on Mar 16 2021 11:30
+@author: Aron, Luleå University of Technology
+"""
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.dates as mdates
+# import scipy.stats as spstats
+# import pandas as pd
+# import os
+# import datetime as dt
+
+import generaltools as gtol
+
+# global variables
+path_data = '../data_observer/xml/' # folder containing the raw data
+path_features = '../data_observer/features_dfs/' # folder containing the features files
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
