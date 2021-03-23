62f501be2643f424eee59a954712fe5abd3ed9d9
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-23 17:28:19 +0100

    start of observer-merge

--- observer-merge.py
+++ observer-merge.py
@@ -4,3 +4,18 @@
 Created on Mar 23 2021 15:48
 @author: Aron, Luleå University of Technology
 """
+# import observer-xml as obsx
+# import observer-uff as obsu
+
+def main():
+    save_raw_data()
+
+def save_raw_data():
+    """ Save a dataframe with raw data
+    Use pickle to save raw data in dataframe.
+    """
+    nodelist = obsx.nodelist()
+    print(nodelist
+
+if __name__ == '__main__':
+    main()
