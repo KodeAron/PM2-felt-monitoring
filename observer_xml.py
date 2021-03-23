b206a07fa858074ff4f4b553f1ee82324d388f2f
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-23 21:18:22 +0100

    sort by date and reset index of meas_df

--- observer_xml.py
+++ observer_xml.py
@@ -54,6 +54,7 @@ def measurements_info(xmdfilename=default_filename+'.xmd'):
     # convert to central european time, UTC+1
     meas_df.MeasDate = meas_df.MeasDate.dt.tz_convert('CET')
     # print(meas_df['MeasDate'][0].tzinfo)
+    meas_df = meas_df.sort_values(by='MeasDate').reset_index(drop=True,inplace=False)
     return meas_df
 
 def plot_signal_from_xmd(IDNode, datestring, xmdfilename=default_filename+'.xmd'):
