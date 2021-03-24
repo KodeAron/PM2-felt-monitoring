e602c9b94e9c1a1e507cfb7cb2939ae84416b6f5
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-24 15:53:50 +0100

    Find dates that does not differ exactly 0 or 12 hours.

--- observer_merge.py
+++ observer_merge.py
@@ -8,6 +8,7 @@ import observer_xml as obsx
 import observer_uff as obsu
 import numpy as np
 import datetime as dt
+import pandas as pd
 
 def main():
     save_raw_data()
@@ -25,7 +26,6 @@ def save_raw_data():
     df = obsx.measurements_info()
 
     df['RawData'] = np.nan
-    print(df)
 
     for pos in uffdfs:
         # add space between roller name and F/D
@@ -35,29 +35,76 @@ def save_raw_data():
         IDNode = next(node['IDNode'] for node in nodelist if node["NodeName"].startswith(position_str))
         print(IDNode)
 
+        # get all row indices that belongs to the node
         noderows = df.loc[(df['IDNode'] == IDNode)].index 
 
-        df.at[noderows,'RawData'] = noderows
+        # print(type(df.iloc[noderows].MeasDate.iloc[0]))
+        # print(type(pos['featuresDF'].MeasDate.iloc[0]))
 
-        print(noderows)
-        print(df.iloc[7:10])
+        dates_df = df.iloc[noderows[0:len(pos['featuresDF'])]]['MeasDate'].reset_index(drop=True)
+        dates_pos = pos['featuresDF']['MeasDate'].reset_index(drop=True)
+        # print(dates_df)
+        # print(dates_pos)
 
-        diff = df.iloc[noderows[0]].MeasDate-pos['featuresDF'].iloc[-1].Datetime
+        # date_match = (dates_df == dates_pos + dates_df-dates_pos == dt.timedelta(hours=12) + \
+        #     dates_pos-dates_df == dt.timedelta(hours=12))
 
-        if diff < dt.timedelta(milliseconds=10):
-            print('less than 10 ms diff')
-        elif diff < dt.timedelta(seconds=1):
-            print('less than 1 s diff')
-        elif diff < dt.timedelta(minute=1):
-            print('less than 1 min diff')
+        diff_array = dates_df-dates_pos
+        # print(diff_array)
 
-        print(diff)
+        acceptable_diff = np.array([dt.timedelta(hours=0),dt.timedelta(hours=12),\
+            dt.timedelta(days=-1,hours=12)], dtype="timedelta64[ms]")
+
+        match_dates = np.isin(diff_array,acceptable_diff)
+
+        if match_dates.all():
+            print("All 'MeasDate' match.")
+        else:
+            print("One or more 'MeasDate' does not match.")
+            for ind in range(len(match_dates)):
+                if not match_dates[ind]:
+                    print(dates_df[ind], end=' vs ')
+                    print(dates_pos[ind])
+
+        # print(date_match)
+
+        # match_0 = df.iloc[noderows[0:len(pos['featuresDF'])]][['MeasDate']].reset_index(drop=True)\
+        #      == pos['featuresDF'][['MeasDate']].reset_index(drop=True)
+             
+        # pos_p12 = pos['featuresDF'][['MeasDate']] + pd.DateOffset(hours=12)
+        # match_p12 = df.iloc[noderows[0:len(pos['featuresDF'])]][['MeasDate']].reset_index(drop=True)\
+        #      == pos_p12.reset_index(drop=True)
+        # pos_m12 = pos['featuresDF'][['MeasDate']] - pd.DateOffset(hours=12)
+        # match_m12 = df.iloc[noderows[0:len(pos['featuresDF'])]][['MeasDate']].reset_index(drop=True)\
+        #      == pos_m12.reset_index(drop=True)
+        # match_df = df.loc[np.all(df.iloc[noderows[0:len(pos['featuresDF'])-1]][['MeasDate']].values == pos['featuresDF'][['MeasDate']].values, axis=0)]
+        # match=pd.DataFrame(columns=['MeasDate'])
+        # match = np.any(match_0['MeasDate'].to_numpy(),match_p12['MeasDate'].to_numpy(),match_m12['MeasDate'].to_numpy())
+        # print(match)
+
+        # df = df.loc[np.all(df.values == df.values, axis=1),:]
+
+        # print(df.iloc[noderows][['MeasDate']].values == pos['featuresDF'][['MeasDate']].values)
+        # df['RawData'] = np.where((df.iloc[noderows][['MeasDate']].values == pos['featuresDF'][['MeasDate']].values),
+        # 'equal', 'nope')
+
+        # print(len(matching_time),'/',len(pos['featuresDF']))
+
+        # df.at[noderows,'RawData'] = noderows
+
+        # print(noderows)
+        # print(df.iloc[7:10])
+
+        # diff = df.iloc[noderows[0]].MeasDate-pos['featuresDF'].iloc[-1].MeasDate
+
+        # if diff < dt.timedelta(milliseconds=10):
+        #     print('less than 10 ms diff')
 
         # df.iloc[0]['RawData'] = 'test'
 
-        # df.set_value()
-
         # print(df[df['RawData']=='test'])
+        
+    print(df)
 
 if __name__ == '__main__':
     main()
