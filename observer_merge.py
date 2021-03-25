a6d3552338104cff630680e4149ba38c1df93a6c
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-25 22:30:25 +0100

    Overlap test moved to "real" function, save_raw_data(). Save list with loaded nodes.

--- observer_merge.py
+++ observer_merge.py
@@ -10,9 +10,11 @@ import numpy as np
 import datetime as dt
 import pandas as pd
 
+picklefilename = '../data_observer/pickles/1aPressT_Acc_ej-nyp_201015-210325'
+
 def main():
-    # save_raw_data()
-    testing_df_join()
+    save_raw_data()
+    # testing_df_join()
 
 def save_raw_data():
     """ Save a dataframe with raw data
@@ -28,6 +30,7 @@ def save_raw_data():
 
     # add data column to df
     df['RawData'] = np.nan
+    loaded_nodes = [] # save which nodes that have been load to prevent loading duplicates.
 
     for pos in uffdfs:
         # get position, i.e. which node
@@ -36,40 +39,52 @@ def save_raw_data():
         position_str = position[0:4] + ' ' + position[4]
         # lookup the ID from the name
         IDNode = next(node['IDNode'] for node in nodelist if node["NodeName"].startswith(position_str))
-        print(position,'>',IDNode)
+        if IDNode in loaded_nodes:
+            raise ValueError('Loaded node that have already been merged.')
+        print(position,'=',IDNode)
+        loaded_nodes.append(IDNode) # save that the node have been loaded
 
         df_unit = df.loc[df['IDNode']==IDNode,:].copy()
-        rawdf = pos['featuresDF']
+        rawdf = pos['df']
+
+        print(rawdf.columns)
 
         # raise warning if data already stored in data column
         if df_unit['RawData'].isnull().values.all():
             print("All in df_unit['RawData'] is nan. Ready to write.")
             print('len(df_unit) =',str(len(df_unit)),':','len(rawdf) =',str(len(rawdf)))
         else:
-            print(df_unit[df_unit['RawData']!=np.nan])
+            print(df_unit[df_unit['RawData'].notnull()])
             raise ValueError('Data appeared in unprocessed measurement info.')
 
         df_unit.drop(labels='RawData',axis=1,inplace=True)
 
-        # join data where time matches exactly
         joined_df = df_unit.join(rawdf.set_index('MeasDate'),on='MeasDate')
-        rawdf['MeasDate'] = rawdf['MeasDate'] + dt.timedelta(hours=12)
-        # join data where time differs by +12 h
-        joined_pos = df_unit.join(rawdf.set_index('MeasDate'),on='MeasDate')
-        rawdf['MeasDate'] = rawdf['MeasDate'] + dt.timedelta(days=-1)# hours=12 already added, net is -12 h
-        # join data where time differs by -12 h
-        joined_neg = df_unit.join(rawdf.set_index('MeasDate'),on='MeasDate')
-        # print(joined_neg)
+        rawdf['MeasDate'] = rawdf['MeasDate'].apply(clock12_to_afternoon)
+        # print('rawdf "converted" to 24h =',rawdf)
+        joined_24 = df_unit.join(rawdf.set_index('MeasDate'),on='MeasDate')
 
-        joined_df.update(joined_pos,errors='raise')
-        joined_df.update(joined_neg,errors='raise')
-        df.update(joined_df,errors='raise')
-        print('joined_df =',str(joined_df))
+        # check for overlaps
+        indices1 = joined_df.loc[joined_df['RawData'].notnull()].index 
+        indices2 = joined_24.loc[joined_24['RawData'].notnull()].index 
+        overlap = []
+        for elem in indices1:
+            if elem in indices2:
+                overlap.append(elem)
+        if len(overlap)>0:
+            print('Warning! Overlap detected. Data may be lost.')
+            print(overlap)
+            print(joined_df.loc[overlap])
+            print(joined_24.loc[overlap])
 
-    print(df)
+        joined_df.update(joined_24)
+        df.update(joined_df)
+
+    df.to_pickle(picklefilename)
+    print(df.head())
 
 def testing_df_join():
-    df = pd.DataFrame({'date': [\
+    df = pd.DataFrame({'MeasDate': [\
                 dt.datetime(2021,3,20,21,2),dt.datetime(2021,3,21,9,16),\
         dt.datetime(2021,3,22,23,54),dt.datetime(2021,3,23,14,31),dt.datetime(2021,3,24,11,41),\
             dt.datetime(2021,3,25,20,1),dt.datetime(2021,3,26,15,0),dt.datetime(2021,3,27,7,33),\
@@ -83,12 +98,12 @@ def testing_df_join():
     #     dt.datetime(2021,3,22,23,17),dt.datetime(2021,3,23,14,11),dt.datetime(2021,3,24,11,13)],\
     #             'data':[np.random.rand() for i in range(8)]})
     # unit 26, in 12-horu clock
-    unit26 = pd.DataFrame({'date': [dt.datetime(2021,3,20,9,2),dt.datetime(2021,3,21,9,16),\
+    unit26 = pd.DataFrame({'MeasDate': [dt.datetime(2021,3,20,9,2),dt.datetime(2021,3,21,9,16),\
         dt.datetime(2021,3,22,11,54),dt.datetime(2021,3,23,2,31),dt.datetime(2021,3,24,11,41),\
             dt.datetime(2021,3,25,8,1),dt.datetime(2021,3,26,3,0),dt.datetime(2021,3,27,7,33)],\
                 'data':[i+0.26 for i in range(8)]})
     # unit 15, in 12-hour clock
-    unit15 = pd.DataFrame({'date': [dt.datetime(2021,3,20,9,17),dt.datetime(2021,3,21,9,16),\
+    unit15 = pd.DataFrame({'MeasDate': [dt.datetime(2021,3,20,9,17),dt.datetime(2021,3,21,9,16),\
         dt.datetime(2021,3,22,11,17),dt.datetime(2021,3,23,2,11),dt.datetime(2021,3,24,11,13),\
             dt.datetime(2021,3,25,8,52),dt.datetime(2021,3,26,12,2),dt.datetime(2021,3,27,12,2)],\
                 'data':[i+0.15 for i in range(8)]})
@@ -120,35 +135,16 @@ def testing_df_join():
         df_unit.drop(labels='data',axis=1,inplace=True)
 
         # print('original other =',other)
-        # joined_df = df_unit.join(other.set_index('date'),on='date')
-        # print('joined_df',joined_df)
-        # other['date'] = other['date'] + dt.timedelta(hours=12)
-        # print('+12 h other =',other)
-        # joined_pos = df_unit.join(other.set_index('date'),on='date')
-        # print('joined_pos',joined_pos)
-        # other['date'] = other['date'] + dt.timedelta(days=-1)# hours=12 already added, net is -12 h
-        # print('-12 h other =',other)
-        # joined_neg = df_unit.join(other.set_index('date'),on='date')
-        # print('joined_neg',joined_neg)
-
-        # joined_df = df.join(other.set_index(['date','unit']),on=['date','unit'])
-        # other['date'] = other['date'] + dt.timedelta(hours=12)
-        # print(other.head())
-        # joined_12 = df.join(other.set_index(['date','unit']),on=['date','unit'])
-        # print(joined_df)
-        # print(joined_12)
-
-        # print('original other =',other)
-        joined_df = df_unit.join(other.set_index('date'),on='date')
+        joined_df = df_unit.join(other.set_index('MeasDate'),on='MeasDate')
         print('joined_df',joined_df)
-        other['date'] = other['date'].apply(clock12_to_afternoon)
+        other['MeasDate'] = other['MeasDate'].apply(clock12_to_afternoon)
         # print('other "converted" to 24h =',other)
-        joined_24 = df_unit.join(other.set_index('date'),on='date')
+        joined_24 = df_unit.join(other.set_index('MeasDate'),on='MeasDate')
         print('joined_24',joined_24)
 
         # check for overlaps
-        indices1 = joined_df.loc[joined_df['data'].notnull()].index 
-        indices2 = joined_24.loc[joined_24['data'].notnull()].index 
+        indices1 = joined_df.loc[joined_df['RawData'].notnull()].index 
+        indices2 = joined_24.loc[joined_24['RawData'].notnull()].index 
         overlap = []
         for elem in indices1:
             if elem in indices2:
