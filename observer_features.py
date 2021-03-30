8c7b7315a3f7762419c1c97d9719e941874f43ff
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-30 10:16:24 +0200

    Remove measurements before 2020-11-04 because they do not ontain data from all 14 nodes.

--- observer_features.py
+++ observer_features.py
@@ -8,6 +8,7 @@ import pandas as pd
 import observer_merge as obsm
 import numpy as np
 import scipy.stats as spstats 
+import datetime as dt
 
 picklefilepath = obsm.picklefilepath # retrieve file location from observer_merge.py
 
@@ -19,16 +20,33 @@ def features():
     # delete (a)larm measurements (StorageReason=1)
     # and group by date
     measdf = pd.read_pickle(picklefilepath)
-
-    print(measdf.columns)
-    print(measdf.index)
+    # print(measdf.columns)
+    # print(measdf.index)
 
     df = measdf[['MeasDate','IDNode','NodeName','StorageReason','Speed','MeasValue','RawData']].copy()
 
-    print(df[df['NodeName']=='P303D'].head()) # to check if StorageReason=1 really is larm, seems so'
+    # print(df[df['NodeName']=='P303D'].head()) # to check if StorageReason=1 really is larm, seems so'
 
     df['rms'] = df['RawData'].apply(vec_rms)
     df['kurtosis'] = df['RawData'].apply(vec_kurtosis)
+
+    # delete larm measurements, i.e. only keep sccheduled (StorageReason=0)
+    df = df[df['StorageReason'] == '0'] #.reset_index(drop=True,inplace=False)
+        #.sort_values(by=['MeasDate','IDNode'])
+
+    # # print measurements that belongs together
+    # print(df[df['MeasDate'].dt.floor(freq = 'D') == dt.datetime(2020,11,4)])
+
+    # remove dates before 2020-11-04
+    df = df[df['MeasDate'] > dt.datetime(2020,11,4)].reset_index(drop=True,inplace=False)
+    print(df)
+
+    # check for missing data
+    print(df[df['RawData'].isnull()])
+
+    # group measurements that were performed close in time
+    df.set_index('MeasDate').groupby(pd.Grouper(freq='H'))
+    print(df)
 
 def vec_rms(data_array):
     val_rms = np.sqrt(np.mean(data_array**2))
