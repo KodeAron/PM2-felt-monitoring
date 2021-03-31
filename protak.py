20aced7c7d50f601bb9d96424666cd5885748e22
Aron <67368318+CodeAron@users.noreply.github.com>
2021-03-31 07:48:27 +0200

    Handles new format of protak files.

--- protak.py
+++ protak.py
@@ -9,32 +9,35 @@ import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 
+protakfilepath = '../data_protak/ProTAK statistics raw PM2 2020-10-30 - 2021-03-26.xlsx'
+
 def main():
-    filename = '../data_protak/ProTAK PM2 Pressektion 201001-210228.xlsx'
+    # filename = '../data_protak/ProTAK PM2 Pressektion 201001-210228.xlsx'
     df = load_data()
-    # print(df[df.REASONDESCRIPTION=='Trimproblem'])
+    # print(df[df.Reason=='Trimproblem'])
     boolean_date_plot(df)
 
 
-def load_data(filename='../data_protak/ProTAK PM2 Pressektion 201001-210228.xlsx',
+def load_data(filename=protakfilepath,
     reasonfilter=['Trimproblem']):
     
     # read operation data from spreadsheet and return as dataframe
-    df = pd.read_excel(filename)
-    # create new df fitlered on REASONDESCRIPTION in reasonfilter
-    # Trimproblem, Massakladd, Hål. More relevant REASONDESCRIPTION ??
-    df_trim = df[df.REASONDESCRIPTION.isin(reasonfilter)].copy().reset_index(drop=True,inplace=False)
+    df = pd.read_excel(filename,header=6)
+    # create new df fitlered on Reason in reasonfilter
+    # Trimproblem, Massakladd, Hål. More relevant Reason ??
+    df = df[df.Reason.isin(reasonfilter)].copy().reset_index(drop=True,inplace=False)
     # remove empty columns
-    df_trim.dropna(axis=1, how='all', inplace=True) 
+    df.dropna(axis=1, how='all', inplace=True) 
+
     # convert to datetime format
-    df_trim['STARTDATE'] = pd.to_datetime(df_trim['STARTDATE'], format='%d-%m-%Y %H:%M:%S')
-    df_trim['ENDDATE'] = pd.to_datetime(df_trim['ENDDATE'], format='%d-%m-%Y %H:%M:%S')
-    return df_trim
+    df['StartDate'] = pd.to_datetime(df['StartDate'], format='%d-%m-%Y %H:%M:%S')
+    df['EndDate'] = pd.to_datetime(df['EndDate'], format='%d-%m-%Y %H:%M:%S')
+    return df
     
 def boolean_date_plot(dataframe):
-    # df_trim= dataframe.set_index(['REASONDESCRIPTION'],\
+    # df_trim= dataframe.set_index(['Reason'],\
     #     drop=True).loc['Trimproblem'].copy() 
-    plt.plot(dataframe.STARTDATE, np.ones(len(dataframe.STARTDATE)),'*', label="")
+    plt.plot(dataframe.StartDate, np.ones(len(dataframe.StartDate)),'*', label="")
     myFmt = mdates.DateFormatter('%d/%m %H:%M') # select format of datetime
     plt.gca().xaxis.set_major_formatter(myFmt)
     plt.show()
@@ -44,7 +47,7 @@ def boolean_date_plot(dataframe):
 
 def check_datetime_for_problem(dataframe,datetime):
     # check if specific point in time had any problem
-    # return boolean value and REASONDESCRIPTION
+    # return boolean value and Reason
 
     return problem_bool, reasondescription
 
