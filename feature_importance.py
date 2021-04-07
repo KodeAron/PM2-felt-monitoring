9752ce17875f65b194663b6bf6cc04f8556bf924
Aron <67368318+CodeAron@users.noreply.github.com>
2021-04-07 17:43:23 +0200

    Created file for feature importance calculation.

---
+++ feature_importance.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- python3
+""" feature importance of merged dataframe
+
+Calculate feature importance on features in the merged dataframe from extractcombiner.
+
+Created on Apr 7 2021 17:42
+@author: Aron, Luleå University of Technology
+"""
