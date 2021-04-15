f56ec1dbc6f79477d0507a510a31a89990aeed2f
Aron <67368318+CodeAron@users.noreply.github.com>
2021-04-15 10:34:36 +0200

    Several ffts in same plot. For chosen node.

--- plot_ffts.py
+++ plot_ffts.py
@@ -11,11 +11,15 @@ Created on Apr 2021 15:40
 # picklefilepath = obsm.picklefilepath
 # or just hard code it for increased speed
 picklefilepath = '../data_observer/pickles/' + '1aPressT_Acc_ej-nyp_201015-210325'
+import feltdata
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import rfft, rfftfreq
 
 # load raw vibration data 
 measdf = pd.read_pickle(picklefilepath)
-print(measdf.columns)
+# print(measdf.columns)
 
 # create a copy (df) with only relevant columns
 df = measdf[['MeasDate','IDNode','NodeName','StorageReason','Speed',\
@@ -29,7 +33,6 @@ nodename = 'P301F'
 df = df[df['NodeName'] == nodename]
 
 # select time interval and remove other measurements
-import feltdata
 felt_df = feltdata.load_data()
 felt_replacements = feltdata.replacement_list(felt_df,\
     first_date=df.iloc[0].MeasDate,last_date=df.iloc[-1].MeasDate)
@@ -39,24 +42,31 @@ df = df[(df['MeasDate'] > felt_replacements[1]) & (df['MeasDate'] < felt_replace
 
 print(df.head())
 
-# select a sample
-sample = df.iloc[0]
+# select samples
+samples = [df.iloc[3], df.iloc[-3]]
+# colors = ['g','r']
+# # create figure
+# fig = plt.figure()
+for sample in samples:
+    label = str(sample.MeasDate)
+    print(label)
+    print('speed =',sample.Speed)
+    # run fft
+    samplerate = float(sample.SampleRate)
+    print('samplerate',samplerate)
+    # N = float(sample.SpectraLines)
+    N = len(sample.RawData)
+    print('N =',N)
+    duration = N/samplerate
+    print('duration =',duration)
+    # N = samplerate * DURATION
 
-# run fft
-from scipy.fft import rfft, rfftfreq
-samplerate = float(sample.SampleRate)
-# N = float(sample.SpectraLines)
-N = len(sample.RawData)
-print(N)
-duration = N/samplerate
-print('duration =',duration)
-# N = samplerate * DURATION
+    yf = rfft(sample.RawData)
+    xf = rfftfreq(N, 1 / samplerate)
 
-yf = rfft(sample.RawData)
-xf = rfftfreq(N, 1 / samplerate)
+    plt.plot(xf, np.abs(yf), label=label,linewidth=0.2)
 
 # plot
-import numpy as np
-import matplotlib.pyplot as plt
-plt.plot(xf, np.abs(yf))
+plt.title(nodename)
+plt.legend()
 plt.show()
